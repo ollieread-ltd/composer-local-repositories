d20985bc977a70be412dd9b0517ad6c4857dd137
Doeke Norg <doekenorg@gmail.com>
2024-08-16 13:50:45 +0200

    Add disabling on `--no-dev`
    - Resolve composer path via the current instance
    - Deduplicate info messages
    - Put some messages behind a `verbose` flag
    - Moved action to `install` and `update` only to be able to intercept the `--no-dev` flag
    - Reading json via JsonFile to avoid errors
    - Fixed plugin name in composer.json
    - Added PHPStan rules and `analyse` script

--- src/LocalRepositoriesPlugin.php
+++ src/LocalRepositoriesPlugin.php
@@ -1,33 +1,109 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Ollieread\ComposerLocalRepositories;
 
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\Factory;
 use Composer\IO\IOInterface;
 use Composer\Json\JsonFile;
+use Composer\Json\JsonValidationException;
+use Composer\Plugin\CommandEvent;
+use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
+use Seld\JsonLint\ParsingException;
 
 class LocalRepositoriesPlugin implements PluginInterface, EventSubscriberInterface
 {
-    /**
-     * @var \Composer\Composer
-     */
     private Composer $composer;
-
-    /**
-     * @var \Composer\IO\IOInterface
-     */
     private IOInterface $io;
-
-    private array $repositories = [];
+    private bool $is_main_composer;
 
     public function activate(Composer $composer, IOInterface $io): void
     {
         $this->composer = $composer;
-        $this->io       = $io;
+        $this->io = $io;
+        // Composer runs copies of itself inside the run. We use this to prevent writing the same output multiple times.
+        $this->is_main_composer = !str_contains(get_class($this), '_composer_tmp');
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            PluginEvents::COMMAND => 'loadRepositories',
+        ];
+    }
+
+    public function loadRepositories(CommandEvent $event): void
+    {
+        if (
+            !in_array($event->getCommandName(), ['install', 'update'], true)
+            || $event->getInput()->getOption('no-dev')
+        ) {
+            return;
+        }
+
+        // Get root path according to Composer (when providing the path to the binary).
+        $rootPath = dirname($this->composer->getConfig()->getConfigSource()->getName());
+        $packagesPath = $rootPath . DIRECTORY_SEPARATOR . 'repositories.json';
+
+        if (!file_exists($packagesPath)) {
+            if ($this->is_main_composer) {
+                $this->io->write('<info>No local repositories.json available</info>', true, IOInterface::VERBOSE);
+            }
+            return;
+        }
+
+        if ($this->is_main_composer) {
+            $this->io->write('<info>Local repositories.json available</info>');
+        }
+
+        $this->loadPackagesFrom($packagesPath);
+    }
+
+    private function loadPackagesFrom(string $path): void
+    {
+        try {
+            $json = new JsonFile($path);
+            $json->validateSchema(JsonFile::LAX_SCHEMA, __DIR__ . '/../resources/composer-repositories-schema.json');
+            $contents = $json->read();
+        } catch (JsonValidationException|ParsingException $e) {
+            $this->io->writeError($e->getMessage());
+            return;
+        }
+
+        if (isset($contents['repositories'])) {
+            // The reverse is important so that their priority matches the order
+            // they're defined in.
+            $repositories = array_reverse($contents['repositories']);
+
+            $this->io->write('<info>Repositories found:</info> ' . count($repositories), true, IOInterface::VERBOSE);
+
+            foreach ($repositories as $name => $config) {
+                $this->loadRepository($name, $config);
+            }
+        }
+    }
+
+    /**
+     * @param array{type: ?string, url: string } $config
+     */
+    private function loadRepository(int|string $name, array $config): void
+    {
+        $manager = $this->composer->getRepositoryManager();
+
+        $repository = $manager->createRepository(
+            $config['type'] ?? 'path', // If there's no type, it's path-based
+            $config,
+            is_string($name) ? $name : null,
+        );
+
+        $this->io->write('<info>Adding repository:</info> ' . $repository->getRepoName());
+
+        $manager->prependRepository($repository);
+
+        $this->io->write('Repository added successfully', true, IOInterface::VERBOSE);
     }
 
     public function deactivate(Composer $composer, IOInterface $io): void
@@ -38,80 +114,5 @@ class LocalRepositoriesPlugin implements PluginInterface, EventSubscriberInterfa
     public function uninstall(Composer $composer, IOInterface $io): void
     {
         // This is intentionally empty
-    }
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'init' => 'loadRepositories',
-        ];
-    }
-
-    /**
-     * @return void
-     * @throws \Composer\Json\JsonValidationException
-     * @throws \JsonException
-     */
-    public function loadRepositories(): void
-    {
-        $rootPath     = dirname(Factory::getComposerFile());
-        $packagesPath = $rootPath . DIRECTORY_SEPARATOR . 'repositories.json';
-
-        if (! file_exists($packagesPath)) {
-            $this->io->writeError('<info>No local repositories.json available</info>');
-        } else {
-            $this->io->writeError('<info>Local repositories.json available</info>');
-
-            $this->loadPackagesFrom($packagesPath);
-        }
-    }
-
-    /**
-     * @throws \Composer\Json\JsonValidationException
-     * @throws \JsonException
-     */
-    private function loadPackagesFrom(string $path): void
-    {
-        // Get the contents of the file
-        $contents = json_decode(file_get_contents($path), true, 512, JSON_THROW_ON_ERROR);
-
-        // Validate the schema, to make sure it's valid
-        JsonFile::validateJsonSchema(
-            $path,
-            $contents,
-            JsonFile::LAX_SCHEMA,
-            __DIR__ . '/../resources/composer-repositories-schema.json'
-        );
-
-        if (isset($contents['repositories'])) {
-            // The reverse is important so that their priority matches the order
-            // they're defined in.
-            $repositories = array_reverse($contents['repositories']);
-
-            $this->io->writeError('<info>Repositories found:</info> ' . count($repositories));
-
-            foreach ($repositories as $name => $config) {
-                $this->loadRepository($name, $config);
-            }
-        }
-    }
-
-    private function loadRepository(int|string $name, array $config): void
-    {
-        $manager = $this->composer->getRepositoryManager();
-
-        $repository = $manager->createRepository(
-            $config['type'] ?? 'path', // If there's no type, it's path-based
-            $config,
-            $name
-        );
-
-        $this->io->writeError('<info>Adding repository:</info> ' . $repository->getRepoName());
-
-        $this->repositories[] = $repository;
-
-        $manager->prependRepository($repository);
-
-        $this->io->writeError('Repository added successfully');
     }
 }
